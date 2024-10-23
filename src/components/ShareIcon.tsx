b114a7284b6bd4eb5a4e2fec3af3940c3b4a17f4
Pierre-Antoine Champin <pierre-antoine@w3.org>
2024-10-23 12:03:13 +0200

    better share icon

--- src/components/ShareIcon.tsx
+++ src/components/ShareIcon.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from "react";
+import { useTranslation } from "react-i18next";
 
 export const ShareIcon: FunctionComponent = () => {
-	return <i style={{ fontSize: "66%" }}>(share)</i>;
+	const { t } = useTranslation();
+	return <abbr title={t('share')}><img alt={t('share')} src="/share-icon.svg" style={{height: "0.7em", marginLeft: "0.3ex"}} /></abbr>;
 }
