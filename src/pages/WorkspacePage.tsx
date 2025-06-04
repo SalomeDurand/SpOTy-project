87403d6f4f1551ee6739ec2ea3ac56a9447fdeae
Salome Durand <salome.durand.adeline4@hotmail.fr>
2025-06-04 12:20:55 +0200

    adding more browser-compatible emoji

--- src/pages/WorkspacePage.tsx
+++ src/pages/WorkspacePage.tsx
@@ -81,7 +81,7 @@ const ContainerItem: FunctionComponent<{
   }
   const tools = "container" in args ? <span className="tools">
     { session.isLoggedIn
-      ? <LinkButton help={t("new folder") + " NOT IMPLEMENTED YET"} onClick={() => {}} disabled={true}>+🖿 </LinkButton>
+      ? <LinkButton help={t("new folder") + " NOT IMPLEMENTED YET"} onClick={() => {}} disabled={true}>🗂️ </LinkButton>
       : null }
     { session.isLoggedIn
       ? <UploadCollection container={args.container} onSuccess={refresh} />
@@ -95,7 +95,7 @@ const ContainerItem: FunctionComponent<{
 
   return <li><details open={true}>
     <summary>
-      🖿 {label} { tools }
+      📂 {label} { tools }
     </summary>
     { unfetched
       ? <ul><li>⏳</li></ul>
@@ -159,7 +159,7 @@ const LeafItem: FunctionComponent<{
 
   return <li><details>
     <summary>
-      🗋  {label} 
+      📄  {label} 
       {" "} ({sentences?.length || "⏳"})
       {errors?.length ? <> {ERR}</> : null}
       <span className="tools">
@@ -180,7 +180,7 @@ const LeafItem: FunctionComponent<{
       ? <ul><li>⏳</li></ul>
       : <ul>
           { errors.map(e => <li key={e}>{ERR} {e}</li>) }
-          { sentences.map(s => <li key={s}>🗩 <SentenceLink uri={s} /></li>) }
+          { sentences.map(s => <li key={s}>💬 <SentenceLink uri={s} /></li>) }
         </ul>
     }
   </details></li>
@@ -228,7 +228,7 @@ const UploadCollection: FunctionComponent<{
 
   return (
     <LinkButton help={t("upload file")} onClick={buttonUploadFile} disabled={converter === null}>
-      +🗋
+      📑
       <input type="file" ref={fileInputRef} accept=".xlsx" multiple={true} onChange={uploadFiles} style={{display: "none"}} />
     </LinkButton>
   );
